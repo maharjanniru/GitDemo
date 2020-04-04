ad239a82408a59a157bc2e6679279a9aca16f950
Niru Maharjan <nmaharjan@deerhold.org>
2020-04-04 11:27:35 +0545

    new line

--- src/org/niru/Main.java
+++ src/org/niru/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Git demo hello world");
+        System.out.println("this is new added line");
     }
 }
